022ed58eeec38e0ab33ceb94a445d3897a112704
Ozge Ozersahin <ozge.ozersahin@skybettingandgaming.com>
2024-11-20 16:50:42 +0000

    added new pngs for workout file

--- src/components/WorkoutPlan/workout.js
+++ src/components/WorkoutPlan/workout.js
@@ -5,13 +5,17 @@ import legCurl from '../../images/leg-curl.png'
 import legLunge from '../../images/leg-lunge.png'
 import bulgarianSplit from '../../images/bulgarian-split.png'
 import barbellDL from '../../images/barbell-deadlift.png'
+import armDumbbell from "../../images/single-arm-dumbbell-row.png"
+import lateralRaise from "../../images/lateral-raise.png"
+import cablelatPulldown from "../../images/cable-lat-pulldown.png"
+import brenchPress from "../../images/brench-press.png"
 
 
 export const Workout = () => {
    return (
     <div>
         <h1>Let's Start Workout!</h1>
-        <p>Here are some exercises for your lower body:</p>
+        <p>LOWER BODY EXERCISE:</p>
         <a href="https://www.youtube.com/watch?v=p5dCqF7wWUw">
             <img src={legPressImage} alt="leg-press" className="image"></img>
         </a>   
@@ -27,7 +31,19 @@ export const Workout = () => {
         <a href="https://www.youtube.com/watch?v=3UwO0fKukRw">
             <img src={barbellDL} alt="barbellDeadlift" className="image"></img>
         </a>
-        <p>Upper Body:</p>
+        <p>UPPER BODY EXERCISE:</p>
+        <a href="https://www.youtube.com/watch?v=ZRSGpBUVcNw">
+            <img src={armDumbbell} alt="armDumbell" className="image"></img> 
+        </a> 
+        <a href="https://www.youtube.com/watch?v=XPPfnSEATJA">
+            <img src={lateralRaise} alt="lateralraise" className="image"></img> 
+        </a>
+        <a href="https://www.youtube.com/watch?v=JGeRYIZdojU">
+            <img src={cablelatPulldown} alt="cablelatpulldown" className="image"></img> 
+        </a>
+        <a href="https://www.youtube.com/watch?v=X3YrlBmjWrY">
+            <img src={brenchPress} alt="brenchpress" className="image"></img> 
+        </a>          
     </div>
    );
 };
